ba57aa214ecd199392f188ea11c030ff644f3456
Carlos M <77166496+jmarquez2nait@users.noreply.github.com>
2021-01-15 09:48:55 -0700

    Update Program.cs

--- My First Plan/My First Plan/Program.cs
+++ My First Plan/My First Plan/Program.cs
@@ -6,46 +6,36 @@ namespace My_First_Plan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Steps for withdrawing money out of an ATM");
-            Console.WriteLine();
-            Console.WriteLine("Please press enter to start the program...");
+            Console.WriteLine("Steps for withdrawing money out of an ATM\n");
+            Console.WriteLine("Please press enter to start the program...\n");
             Console.Read();
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 1");
-            Console.WriteLine("Insert debit card into card slot.");
-            Console.WriteLine();
+            Console.WriteLine("Insert debit card into card slot.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 2");
-            Console.WriteLine("Input security access code or pin.");
-            Console.WriteLine();
+            Console.WriteLine("Input security access code or pin.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 3");
-            Console.WriteLine("Select withdraw money.");
-            Console.WriteLine();
+            Console.WriteLine("Select withdraw money.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 4");
-            Console.WriteLine("Select account.");
-            Console.WriteLine();
+            Console.WriteLine("Select account.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 5");
-            Console.WriteLine("Input amount you wish to withdraw.");
-            Console.WriteLine();
+            Console.WriteLine("Input amount you wish to withdraw.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 6");
-            Console.WriteLine("Confirm your transaction.");
-            Console.WriteLine();
+            Console.WriteLine("Confirm your transaction.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 7");
-            Console.WriteLine("Take your money out.");
-            Console.WriteLine();
+            Console.WriteLine("Take your money out.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 8");
-            Console.WriteLine("Exit ATM.");
-            Console.WriteLine();
+            Console.WriteLine("Exit ATM.\n");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Step 9");
-            Console.WriteLine("Take your card out.");
-            Console.WriteLine();
+            Console.WriteLine("Take your card out.\n");
             Console.ReadLine();
 
             string myName = "Carlos Marquez";
